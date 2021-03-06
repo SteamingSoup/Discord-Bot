97fb23a085fc1e2a7257954d9802a033310333a9
Nick <67605183+SteamingSoup@users.noreply.github.com>
2021-03-06 12:25:37 -0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -3,34 +3,55 @@ import random
 
 TOKEN = "~Token goes here~" # or use .env method if you aren't irresponsible
 
+
+client = discord.Client()
+
+# command used to call bot. Example: !r d20
 call_bot = "!"
 
-
+# lets you know that the bot is ready to roll some dice
+@client.event
+async def on_ready():
+    print("Bot Boi Online")
+    
+    
+    
 @client.event
 async def on_message(message):
-  if message.author == client.user:
+  if message.author == client.user: # Need this so bot doesn't respond to itself
     return
   
-  def multi_dice():
-    if message.content.startswith('{0}d'.format(call_bot)):
-      multi_dice.nickname = message.author.display_name
-      
+  """
+  Will want a function that will roll dx when the bot is called with !, where x is the number of sides on die
+  Will also need a way to handle errors, and give a !help function to explain how to use the bot. 
+  """
+  def dice():
+    if message.content.startswith('{}r'.format(call_bot)):
+      dice.nickname = message.author.display_name
+      #!dx where x is the number of sides so sides = message.content[2:], which would be x
       sides = message.content[2:]
-      
+      #the number of sides needs to be a integer for randint function
       sides_int = int(sides)
-      
+      #receive random number between 1 and the number of sides of the dice
       roll = random.randint(1, sides_int)
-      
-      multi_dice.final = str(roll)
-      
-      if sides == "20" and multi_dice.final == "1":
-        multi_dice.message = "Dang, {0} rolled a 1. Sucks to suck".format(multi_dice.nickname)
+      #adding text for when players roll 1 or 20 
+      if sides == "20" and dice.final == "1":
+        dice.message = "Dang, {} rolled a 1. Sucks to suck".format(dice.nickname)
         
-      elif sides == "20" and multi_dice.final == "20":
-        multi_dice.message = "Awesome, {0} rolled a nat 20".format(multi_dice.nickname)
+      elif sides == "20" and dice.final == "20":
+        dice.message = "OwO what's this? {} rolled a 20. Easy life just get the roll".format(dice.nickname)
         
       else:
-        multe_dice.message = "{0} rolled a {1}".format(multi_dice.nickname, multi_dice.final)
+        dice.message = "{} rolled a {}".format(dice.nickname, dice.final)
+        
+   
       
-      
-      
+   # !help function to explain bot usage
+   if message.conent.startwith('{}help'.format(call_bot)):
+      await message.channel.send("*!help*\n"
+                                 "\n"
+                                 "Use ! to call bot"
+                                 "\n"
+                                 "Bot rolls a dx die where x is the sides"
+                                 "\n"
+                                 "Example: !d20 will roll a d20"
